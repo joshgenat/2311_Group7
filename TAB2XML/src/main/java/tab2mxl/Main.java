25ada351a3c58831edaec1218bd8538e29c7484d
uthithmenonravitharan <uthithmenonravitharan@localhost>
2021-01-13 14:28:31 -0500

    Checking

--- TAB2XML/src/main/java/tab2mxl/Main.java
+++ TAB2XML/src/main/java/tab2mxl/Main.java
@@ -3,9 +3,11 @@ package tab2mxl;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Group 7 Created!");
 		System.out.println("test push");
 		System.out.println("test push 2");
 		System.out.println("test push by josh");
+		System.out.println("Did i finally get it to work?");
 	}
 }
