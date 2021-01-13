12aed71cf76542d1397e205cdffcefafb4b703d7
Joshua Genat <joshua_genat@hotmail.com>
2021-01-13 13:04:05 -0500

    test by josh

--- TAB2XML/src/main/java/tab2mxl/Main.java
+++ TAB2XML/src/main/java/tab2mxl/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Group 7 Created!");
-		System.out.println("test");
+		System.out.println("this is a test push");
 	}
 }
