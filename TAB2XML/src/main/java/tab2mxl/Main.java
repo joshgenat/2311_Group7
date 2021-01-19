c9f044ebe4c72e14698c70fa708dbed6a3e0efc6
Joshua Genat <joshua_genat@hotmail.com>
2021-01-19 16:04:37 -0500

    New Class

--- TAB2XML/src/main/java/tab2mxl/Main.java
+++ TAB2XML/src/main/java/tab2mxl/Main.java
@@ -7,15 +7,6 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Group 7 Created!");
-		System.out.println("test push");
-		System.out.println("test push 2");
-		System.out.println("test push by josh");
-		System.out.println("Did i finally get it to work?");
-		System.out.println("test push by uthith");
-		System.out.println("test push by nicolae");
-		
-		
-		System.out.println("test (Jan. 16)");
 		
 		/*
 		 * Just a sample of how we can get the user to tell us which instrument, they are going to enter the tab
---
+++ TAB2XML/src/main/java/tab2mxl/Test.java
@@ -0,0 +1,5 @@
+package src.main.java.tab2mxl;
+
+public class Test {
+	int x;
+}
