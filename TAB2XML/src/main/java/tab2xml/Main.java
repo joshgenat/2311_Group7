e393c1a1d68352308f33676b23f8fff32cf80767
Joshua Genat <joshua_genat@hotmail.com>
2021-01-24 14:36:47 -0500

    test

--- TAB2XML/src/main/java/tab2xml/Main.java
+++ TAB2XML/src/main/java/tab2xml/Main.java
@@ -7,16 +7,8 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Group 7 Created!");
-		System.out.println("New Test by Josh");
-		System.out.println("Test by Uthith");
-		System.out.println("test");
-		System.out.println("test2");
-		System.out.println("Hey guys");
-		System.out.println("Nicolae");
 		
-	
-		
-		System.out.println("lab task 2");
+		System.out.println("new test branch");
 	}
 }
 		
