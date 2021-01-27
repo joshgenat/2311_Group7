b31b427869e82e1ae6196591925e517115a84665
Nicolae_Semionov <nicolae@my.yorku.ca>
2021-01-27 11:49:45 -0500

    test commit 2

--- TAB2XML/src/main/java/tab2xml/Main.java
+++ TAB2XML/src/main/java/tab2xml/Main.java
@@ -4,11 +4,6 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static char guitarConverter(char[][] in) {
-		for(int i = 0; i < in[].size;)
-		
-		return 'A';
-	}
 	
 	public static void main(String[] args) {
 		
