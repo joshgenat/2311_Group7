1d65f7545ed82ce7021223d06c13aca43a7f01ee
Andy Lin <www.andy@hotmail.ca>
2021-01-27 12:55:05 -0500

    test

--- TAB2XML/src/main/java/tab2xml/Main.java
+++ TAB2XML/src/main/java/tab2xml/Main.java
@@ -9,11 +9,7 @@ public class Main {
 		
 		
 		GUI_2 test = new GUI_2();
-<<<<<<< HEAD
-		System.out.println("Andy");
-=======
 		System.out.println("test by josh");
->>>>>>> branch 'master' of https://github.com/joshgenat/2311_Group7
 	}
 }
 		
