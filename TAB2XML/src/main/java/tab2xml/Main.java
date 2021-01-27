34cd607d971eb1432d7a67eeab6268287b3cb1da
Uthith18 <uthith18@my.yorku.ca>
2021-01-27 12:09:00 -0500

    Lab test 2

--- TAB2XML/src/main/java/tab2xml/Main.java
+++ TAB2XML/src/main/java/tab2xml/Main.java
@@ -10,9 +10,7 @@ public class Main {
 		
 		GUI_2 test = new GUI_2();
 		System.out.println("Hey Uthithmenon and Josh and friends");
-		System.out.println("Hey Uthithmenon and Josh (x4)");
-		System.out.println("Hey Uthithmenon and Josh (x3)");
-		System.out.println("Hey Uthithmenon and Josh (x2)");
+		System.out.println("Andy is a great guy");
 		System.out.println("Nicolae");
 	}
 }
