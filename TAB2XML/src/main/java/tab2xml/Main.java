98a96b7ee36cf451fcd64eab849064016689d79b
Joshua Genat <joshua_genat@hotmail.com>
2021-01-27 12:15:22 -0500

    test

--- TAB2XML/src/main/java/tab2xml/Main.java
+++ TAB2XML/src/main/java/tab2xml/Main.java
@@ -10,7 +10,7 @@ public class Main {
 		
 		GUI_2 test = new GUI_2();
 		System.out.println("Hey Uthithmenon and Josh (x5)");
-		System.out.println("test");
+		System.out.println("test x2");
 	}
 }
 		
