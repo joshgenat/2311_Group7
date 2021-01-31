adeb3a3b79ec22c1b99bc7c927906f250921ca58
Andy Lin <www.andy@hotmail.ca>
2021-01-31 14:15:12 -0500

    tab fixes

--- TAB2XML/src/main/java/tab2xml/Tab.java
+++ TAB2XML/src/main/java/tab2xml/Tab.java
@@ -2,6 +2,7 @@ package tab2xml;
 
 import java.util.ArrayList;
 
+
 public class Tab {
 
 	ArrayList<TabNodes> nodes; // ArrayList of the lines of the tab
@@ -13,13 +14,16 @@ public class Tab {
 		boolean typed = false;
 		pass = new ArrayList<Object>();
 		nodes = new ArrayList<TabNodes>();
+		
 		for(int i = 0; i < lines.size(); i++) {
 			
 			//Checks if Lines are actually tab nodes or just lyrics 
 			if(read && lines.get(i).toString().contains("-") && lines.get(i).toString().lastIndexOf('|') - lines.get(i).toString().indexOf('|') > 0) {
+				if(pass.size() < 6) {
 				pass.add(lines.get(i));
+				}
 			}
-			else if(read) {
+			else if(read || pass.size() > 6) {
 				if(typed == false) {
 					Type = "Guitar";
 					for(int j = 0; j < pass.size(); j++) {
@@ -30,9 +34,16 @@ public class Tab {
 					}
 					typed = true;
 				}
+				if(pass.size() > 6) {
+					nodes.add(new TabNodes(pass));
+					pass = new ArrayList<Object>();
+				}
+				else {
 				nodes.add(new TabNodes(pass));
 				pass = new ArrayList<Object>();
 				read = false;
+				}
+
 			}
 			else if(lines.get(i).toString().contains("-") && lines.get(i).toString().lastIndexOf('|') - lines.get(i).toString().indexOf('|') > 0) { 
 				pass.add(lines.get(i));
