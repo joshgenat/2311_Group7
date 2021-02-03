850407ff9f47a0da24c38c7a54afab3adf7d53c5
Andy Lin <www.andy@hotmail.ca>
2021-02-03 13:37:38 -0500

    test

--- TAB2XML/src/main/java/tab2xml/Main.java
+++ TAB2XML/src/main/java/tab2xml/Main.java
@@ -7,18 +7,11 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		
-<<<<<<< HEAD
-		GUI test = new GUI();
-		
-		System.out.println("lab test");
-=======
-
+	
 		//GUI test = new GUI();
 		GUI test = new GUI();
 		
 		System.out.println("nicolae lab test");
->>>>>>> branch 'develop' of https://github.com/joshgenat/2311_Group7
 
 	}
 }
