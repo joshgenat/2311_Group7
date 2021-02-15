d7b225a1937bc3ff84e346f49a8e71e010340139
Nicolae_Semionov <nicolae@my.yorku.ca>
2021-02-15 16:18:12 -0500

    Updated guitar and bass converters

--- TAB2XML/src/tab2xml/GuitarConverter.java
+++ TAB2XML/src/tab2xml/GuitarConverter.java
@@ -3,13 +3,19 @@ package tab2xml;
 public class GuitarConverter {
 	
 	//Main
+	
 	public static void main(String args[]) {
-		char[][] test = {
-				{'1', '-', '-', '-', '5', '-'},
-				{'-', '2', '-', '4', '-', '6'},
-				{'-', '-', '3', '-', '-', '-'}
+		char[][] test1 = {
+				{'0', '0', '0', '0', '0', '0'},
+				{'7', '7', '7', '7', '7', '7'},
+				{'8', '8', '8', '8', '8', '8'}
 		};
-		String[][] notes = Converter(test);
+		char[][] test2 = {
+				{'0', '0', '0', '0', '0', '0', '0'},
+				{'7', '7', '7', '7', '7', '7', '7'},
+				{'8', '8', '8', '8', '8', '8', '8'}
+		};
+		String[][] notes = Converter(test2);
 		
 		for(int i = 0; i < notes.length; i++) {
 			for(int j = 0; j < notes[i].length; j++) {
@@ -21,6 +27,8 @@ public class GuitarConverter {
 		//System.out.println(test[0][1]);
 	}
 	
+	
+	
 	/*
 	 * Returns 2D array of notes, stored as strings
 	 * argument is a 2D array that has been parsed from a tab
@@ -29,12 +37,14 @@ public class GuitarConverter {
 	 * -----0----	the characters and numbers, char[rows][columns]
 	 * -2---0----	example, first column {'1', '-', '-', '-', '5', '-'}
 	 * -----0----
-	 * ---3------
+	 * 5--3------
 	 * -------9-- 
 	 * 
 	 * @param	in	2d char array parsed from tab format
 	 * @return	out	2d String array of notes at their corresponding positions on the tab
 	 */
+	
+	
 	public static String[][] Converter(char[][] in) { 
 		String[][] out;
 		String tmp; 
@@ -45,17 +55,14 @@ public class GuitarConverter {
 			for(j = 0; j < out[i].length; j++) {
 				tmp = "";
 				for(j2 = 0; j2 < out[i].length; j2++) {
-					if(j2 == j)
-						tmp = tmp + in[i][j];
+					if(in[i][j] == '-') // j2 == j
+						out[i][j] = "-";	//tmp = tmp + in[i][j]; 
 					else
-						tmp = tmp + '-';
+						out[i][j] = IndexToNote(i, j, in[i][j]); //tmp = tmp + '-';
 				}
-				out[i][j] = StringToNote(tmp);
+				//out[i][j] = StringToNote(tmp);
 			}
-			
-			
 		}
-		
 		
 		return out;
 	}
@@ -65,8 +72,95 @@ public class GuitarConverter {
 	 * @param	in	String representing a column of characters from a tab e.g "-1-4--"
 	 * @return		Note string, corresponding to tab
 	 */
-	public static String StringToNote(String in) {
+	
+	public static String IndexToNote(int i, int j, char c) {
+		int k = CharToInt(c);
+		int rem = (4 + k + 5 * j) % 12;
 		
+		return rem + IntToNote(rem) + ((28 + 5 * j + k) / 12);
+	}
+	
+	public static String IntToNote(int a) {
+		switch(a) {
+			case 0:
+				return "C";
+			case 1:
+				return "C#";
+			case 2:
+				return "D";
+			case 3:
+				return "D#";
+			case 4:
+				return "E";
+			case 5:
+				return "F";
+			case 6:
+				return "F#";
+			case 7:
+				return "G";
+			case 8:
+				return "G#";
+			case 9:
+				return "A";
+			case 10:
+				return "A#";
+			case 11:
+				return "B";
+			default:
+				return null;
+		}
+	}
+	
+	public static int CharToInt(char c) {
+		switch(c) {
+			case '0':
+				return 0;
+			case '1':
+				return 1;
+			case '2':
+				return 2;
+			case '3':
+				return 3;
+			case '4':
+				return 4;
+			case '5':
+				return 5;
+			case '6':
+				return 6;
+			case '7':
+				return 7;
+			case '8':
+				return 8;
+			case '9':
+				return 9;
+			case '!':
+				return 10;
+			case '@':
+				return 11;
+			case '#':
+				return 12;
+			case '$':
+				return 13;
+			case '%':
+				return 14;
+			case '^':
+				return 15;
+			case '&':
+				return 16;
+			case '*':
+				return 17;
+			case '(':
+				return 19;
+			case ')':
+				return 20;
+			case '=':
+				return 21;
+			default:
+				return '\0';
+		}
+	}
+	
+	public String StringToNote(String in) {
 		switch(in) {
 			case "0-----":
 				return "E4";
@@ -227,6 +321,5 @@ public class GuitarConverter {
 			default:
 				return "-";
 		}
-		
 	}
 }
