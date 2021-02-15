54a82aa5ceb80bc038c9792bb116ffe5cbb40c7d
Nicolae_Semionov <nicolae@my.yorku.ca>
2021-02-15 00:07:49 -0500

    Bass and guitar converters now allow for any number of string

--- TAB2XML/src/main/java/tab2xml/BassConverter.java
+++ TAB2XML/src/main/java/tab2xml/BassConverter.java
@@ -3,16 +3,16 @@ package tab2xml;
 public class BassConverter {
 	
 	//Main
-	/*
+	
 	public static void main(String args[]) {
 		char[][] test4 = {
-				{'1', '-', '-', '6'},
-				{'-', '2', '4', '-'},
-				{'-', '-', '3', '-'}
+				{'0', '0', '0', '0'},
+				{'7', '7', '7', '7'},
+				{'8', '8', '8', '8'}
 		};
 		char[][] test5 = {
-				{'4', '-', '-', '-', '-'},
-				{'3', '-', '7', '-', '-'},
+				{'0', '0', '0', '0', '0'},
+				{'1', '1', '1', '1', '1'},
 				{'-', '-', '8', '-', '5'}
 		};
 		
@@ -35,7 +35,7 @@ public class BassConverter {
 			
 		//System.out.println(test[0][1]);
 	}
-	*/
+	
 	
 	/*
 	 * Returns 2D array of notes, stored as strings
@@ -60,17 +60,109 @@ public class BassConverter {
 			for(j = 0; j < out[i].length; j++) {
 				tmp = "";
 				for(j2 = 0; j2 < out[i].length; j2++) {
-					if(j2 == j)
-						tmp = tmp + in[i][j];
+					if(in[i][j] == '-') // j2 == j
+						out[i][j] = "-";	//tmp = tmp + in[i][j]; 
 					else
-						tmp = tmp + '-';
+						out[i][j] = IndexToNote(i, j, in[i][j]); //tmp = tmp + '-';
 				}
-				out[i][j] = StringToNote(tmp, in[0].length);
+				//out[i][j] = StringToNote(tmp);
 			}
 		}
 		
-		
 		return out;
+	}
+	
+	/*
+	 * Returns a note string that corresponds to the tab column
+	 * @param	in	String representing a column of characters from a tab e.g "-1-4--"
+	 * @return		Note string, corresponding to tab
+	 */
+	
+	public static String IndexToNote(int i, int j, char c) {
+		int k = CharToInt(c);
+		int rem = (4 + k + 5 * j) % 12;
+		
+		return rem + IntToNote(rem) + ((16 + 5 * j + k) / 12);
+	}
+	
+	public static String IntToNote(int a) {
+		switch(a) {
+			case 0:
+				return "C";
+			case 1:
+				return "C#";
+			case 2:
+				return "D";
+			case 3:
+				return "D#";
+			case 4:
+				return "E";
+			case 5:
+				return "F";
+			case 6:
+				return "F#";
+			case 7:
+				return "G";
+			case 8:
+				return "G#";
+			case 9:
+				return "A";
+			case 10:
+				return "A#";
+			case 11:
+				return "B";
+			default:
+				return null;
+		}
+	}
+	
+	public static int CharToInt(char c) {
+		switch(c) {
+			case '0':
+				return 0;
+			case '1':
+				return 1;
+			case '2':
+				return 2;
+			case '3':
+				return 3;
+			case '4':
+				return 4;
+			case '5':
+				return 5;
+			case '6':
+				return 6;
+			case '7':
+				return 7;
+			case '8':
+				return 8;
+			case '9':
+				return 9;
+			case '!':
+				return 10;
+			case '@':
+				return 11;
+			case '#':
+				return 12;
+			case '$':
+				return 13;
+			case '%':
+				return 14;
+			case '^':
+				return 15;
+			case '&':
+				return 16;
+			case '*':
+				return 17;
+			case '(':
+				return 19;
+			case ')':
+				return 20;
+			case '=':
+				return 21;
+			default:
+				return '\0';
+		}
 	}
 	
 	/*
