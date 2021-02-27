81593aeb38a1f1e6475ae6984fd4c73350e2b4cd
Uthith18 <uthith18@my.yorku.ca>
2021-02-27 02:59:23 -0500

    divivers and duration

--- TAB2XML/src/main/java/tab2xml/DrumDuration.java
+++ TAB2XML/src/main/java/tab2xml/DrumDuration.java
@@ -2,25 +2,63 @@ package tab2xml;
 
 public class DrumDuration {
 	
-	int durationcount;
-
+	 int durationcount;
+	 boolean breaksDivider = false;
 	
-	public int NoteDurationLength(int col,  int nextCol, int nextNextCol)
+	public int NoteDurationLength(int col,  int nextCol, int nextNextCol, int [] dividers)
 	{
+		
 		if (col != nextCol)
 		{
-			durationcount = nextCol- col;
+			for (int i = 0; i < dividers.length; i++) 
+			{
+				if ((dividers[i] < col) && (nextCol > dividers[i+1]) && (col< dividers[i+1]))
+				{
+					durationcount = dividers[i+1] - col; 
+					breaksDivider  = true;
+					
+				}
+				
+				
+				if (breaksDivider == false)
+				{
+					durationcount = nextCol - col; 
+					breaksDivider = true;
+					
+				}
+				
+			}
+				
+			
 		}
-	
+		
 		if (col == nextCol)
 		{
 			
-			durationcount = nextNextCol - col;
+			for (int i = 0; i < dividers.length; i++) 
+			{
+				if ((dividers[i] < col) && (nextNextCol > dividers[i+1]) && (col< dividers[i+1]))
+				{
+					durationcount = dividers[i+1] - col; 
+					breaksDivider  = true;
+					
+				}
+				
+				if (breaksDivider == false)
+				{
+					durationcount = nextNextCol - col;
+					breaksDivider = true;
+					
+				}
+			}
+			
 		}
+		
 		
 	
 	return durationcount;
 	}
+
 
 
 
