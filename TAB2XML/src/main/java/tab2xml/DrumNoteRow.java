44a9967029c48d15402b0ee13da41566d3781748
Uthith18 <uthith18@my.yorku.ca>
2021-02-28 11:45:30 -0500

    Changed all arrays to arraylist

--- TAB2XML/src/main/java/tab2xml/DrumNoteRow.java
+++ TAB2XML/src/main/java/tab2xml/DrumNoteRow.java
@@ -1,28 +1,36 @@
 package tab2xml;
 
+import java.util.ArrayList;
+
 public class DrumNoteRow {
 
-	int [] rowCoordinates;
-	int [] colCoordinates;
+	
+	ArrayList<Integer> rowCoordinate;
+	ArrayList<Integer> colCoordinate;
+
 	int counter = 0;
 	boolean mutipleVoices = false;
 	boolean checker = false; 
 	int oldDivider = 0;
 	int currentDivider = 0;
 	
-	int [] RowReader(char [][] drumTab, int [] rowSymbols)
+	ArrayList <Integer> RowReader(char [][] drumTab, int [] rowSymbols)
 	{
 		counter = 0;
 		mutipleVoices = false;
-		rowCoordinates = new int[200];
-		colCoordinates = new int[200];
+		rowCoordinate = new ArrayList<Integer>();
+		colCoordinate = new ArrayList<Integer>();
 		checker = false; 
 		oldDivider = 0;
 		currentDivider = 0;
 		
 		
 		
-		
+		/*
+		 * This for loop which goes through the tab, and finds the column values for the barlines 
+		 * The first two if statement find the column barline value and put the value in currentDivider, since a tab has more than one barline
+		 * we put the old barline column value inside oldDivider. 
+		 */
 		for (int col = 0; col<drumTab[0].length; col++)
 		{
 		     for (int row = drumTab.length - 1; row >= 0 ; row--)
@@ -43,14 +51,9 @@ public class DrumNoteRow {
 		
 		
 
-		 		if (checker == false)
-		 	{
-		
-		
-		
-		
-		
-		
+	if (checker == false)
+	{
+
 		for (int col1 = oldDivider; col1 < currentDivider; col1++)
         {
 			
@@ -59,14 +62,17 @@ public class DrumNoteRow {
             	//if (rowSymbols[row] == 8) means voice two
             	if ((drumTab[row1][col1] == 'x' || drumTab[row1][col1] == 'o')  && (rowSymbols[row1] != 8))
             	{
-            		rowCoordinates[counter] = row1;
-            		colCoordinates[counter] = col1;
-            		counter++;
+            		rowCoordinate.add(row1);
+            		colCoordinate.add(col1);
+
             	}
             }
             
         }
-		 // run the loop a second time, now to find the notes in voice 2 
+		 
+		
+		
+		// run the loop a second time, now to find the notes in voice 2 
 		
 		for (int col2 = oldDivider; col2 < currentDivider; col2++)
         {
@@ -78,17 +84,15 @@ public class DrumNoteRow {
             	if(((drumTab[row2][col2] == 'x' || drumTab[row2][col2] == 'o') && (rowSymbols[row2]== 8)) && mutipleVoices == false)
             	{
             		mutipleVoices = true;
-            		rowCoordinates[counter] = 100;
-            		colCoordinates[counter] = 100;
-            		counter++;
+            		rowCoordinate.add(1000);
+            		colCoordinate.add(1000);
             		
             	}
             	
             	 if ((drumTab[row2][col2] == 'x' || drumTab[row2][col2] == 'o')  && (rowSymbols[row2]== 8))
             	{
-            		rowCoordinates[counter] = row2;
-            		colCoordinates[counter] = col2;
-            		counter++;
+            		rowCoordinate.add(row2);
+             		colCoordinate.add(col2);
             	}
             
             }
@@ -97,7 +101,7 @@ public class DrumNoteRow {
 		 	}
 		     }
 		}
-		return rowCoordinates;
+		return rowCoordinate;
 		
 		
 		
