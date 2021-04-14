9b08440375464ec0669dd4c4dcdfedc12af92daf
Andy Lin <www.andy@hotmail.ca>
2021-04-13 22:02:46 -0400

    Merge

--- TAB2XML/src/test/java/testing/ControllerTester.java
+++ TAB2XML/src/test/java/testing/ControllerTester.java
@@ -15,7 +15,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
+//
 @ExtendWith(ApplicationExtension.class)
 public class ControllerTester {
 	
